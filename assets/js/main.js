7e40fb7dbd88d5aa704dda22561e4edf9791b47e
Nermin Bosnjak <nermin@MacBook-Pro.local>
2021-09-14 11:10:26 +0200

    Added mobile menu

---
+++ assets/js/main.js
@@ -0,0 +1,36 @@
+const nav = document.querySelector("header nav");
+const items = Array.from(nav.querySelectorAll("li"));
+const hamb = document.querySelector(".hamb-menu");
+
+const tl2 = gsap.timeline({ paused: true });
+
+gsap.to(items, { duration: 0, opacity: 0, y: 50 });
+tl2
+  .to(nav, {
+    duration: 1,
+    width: "100%",
+    ease: "Expo.easeInOut",
+  })
+  .to(
+    items,
+    {
+      opacity: 1,
+      y: 0,
+      stagger: 0.1,
+    },
+    "-=.2"
+  );
+
+tl2.reverse();
+
+hamb.addEventListener("click", (e) => {
+  nav.classList.toggle("active");
+
+  if (nav.classList.contains("active")) {
+    hamb.children[0].classList = "bi bi-x";
+  } else {
+    hamb.children[0].classList = "bi bi-list";
+  }
+  document.body.classList.toggle("open");
+  tl2.reversed(!tl2.reversed());
+});
