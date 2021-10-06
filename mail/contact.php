3f32e33e6c213eb180bb7e6d80f6f72fe03fcd5a
Nermin Bosnjak <nermin@MacBook-Pro.local>
2021-10-07 00:16:44 +0200

    Test9

--- mail/contact.php
+++ mail/contact.php
@@ -1,21 +1,22 @@
 <?php
-$contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
+echo $_POST['name'];
+// $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 
-if ($contentType === "application/json") {
-  //Receive the RAW post data.
-  $content = trim(file_get_contents("php://input"));
+// if ($contentType === "application/json") {
+//   //Receive the RAW post data.
+//   $content = trim(file_get_contents("php://input"));
 
-  $decoded = json_decode($content, true);
+//   $decoded = json_decode($content, true);
 
-  printf($decoded);
+//   printf($decoded);
 
-  //If json_decode failed, the JSON is invalid.
-  if(! is_array($decoded)) {
-    echo "There is an error in JSON";
-  } else {
-    echo "Some error happened";
-  }
-}
+//   //If json_decode failed, the JSON is invalid.
+//   if(! is_array($decoded)) {
+//     echo "There is an error in JSON";
+//   } else {
+//     echo "Some error happened";
+//   }
+// }
 
 // $data = json_decode($_POST);
 
