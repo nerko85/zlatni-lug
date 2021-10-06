fccede1a8fe47266bc8a099b7e97faaaeeead6ad
Nermin Bosnjak <nermin@MacBook-Pro.local>
2021-10-07 00:19:44 +0200

    Test10

--- mail/contact.php
+++ mail/contact.php
@@ -1,5 +1,11 @@
 <?php
-echo $_POST['name'];
+header("Content-Type: application/json"); // Since we are sending a JSON response here (not an HTML document), set the MIME Type to application/json
+//Because you are posting the data via fetch(), php has to retrieve it elsewhere.
+$json_str = file_get_contents('php://input');
+//This will store the data into an associative array
+$json_obj = json_decode($json_str, true);
+
+echo $json_obj;
 // $contentType = isset($_SERVER["CONTENT_TYPE"]) ? trim($_SERVER["CONTENT_TYPE"]) : '';
 
 // if ($contentType === "application/json") {
